d9ae404e053a536afe9f738416ca0c2eda3b8341
Chris Devereux <chrisd(a)damplus.com>
2016-12-02 16:57:19 +0000

    feat(core): Add requestProp middleware factory
    affects: reboot-html-client, reboot-core, reboot-integration-tests, reboot-server

--- packages/core/src/middlewares.request.tsx
+++ packages/core/src/middlewares.request.tsx
@@ -9,3 +9,10 @@ export function requestProps<Required, Emitted>(fn: RequestFn<Required, Emitted>
   return (req: Required & MountRequest, next: NextFn<Required & Emitted & MountRequest>) =>
     Promise.resolve(fn(req)).then(emitted => next(assign({}, req, emitted)))
 }
+
+export function requestProp<Key extends string, Required, Emitted>(key: Key, fn: RequestFn<Required, Emitted>): Middleware<Required, Record<Key, Emitted>> {
+  return async (req: Required & MountRequest, next: NextFn<Required & Record<Key, Emitted> & MountRequest>) => {
+    const emitted = { [key as string]: await fn(req) } as any as Record<Key, Emitted>
+    return next(assign({}, req, emitted))
+  }
+}
