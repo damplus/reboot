ef0ff203eaf4b51dec8ceff90fcad33a731e36ec
Chris Devereux <chrisd(a)damplus.com>
2017-01-09 10:23:22 +0000

    fix(core): Handle constant withDefault value correctly

--- packages/core/src/resource.ts
+++ packages/core/src/resource.ts
@@ -140,7 +140,7 @@ export class MissingAsyncValue extends AsyncValue<never> {
   }
 
   withDefault<T>(defaultVal: T  | ((x: MissingAsyncValue) => T)) {
-    const value = (typeof defaultVal === 'function') ? defaultVal(this) : false
+    const value = (typeof defaultVal === 'function') ? defaultVal(this) : defaultVal
     return new PresentAyncValue({ status: 'loaded', value })
   }
 }
@@ -166,7 +166,7 @@ export class Resource<T> {
   private key: string
 
   constructor(opts: { key: string, store: Store<{}> }) {
-    this.store = opts.store.addReducer(opts.key, reduceHTTPResource)
+    this.store = opts.store.addReducer(opts.key, httpResourceReducer(opts.key))
     this.key = opts.key
   }
 
@@ -202,14 +202,22 @@ export class Resource<T> {
   }
 
   fetchMany(keys: string[], fetchImpl: () => Promise<{ [key: string]: T }>): void {
-    this.store.dispatch<ResourceAction>({ type: 'http:fetch:start', keys })
+    keys = keys.filter(k => {
+      const state = this.store.getState()[this.key][k]
+      return !(state && state.status === 'loading')
+    })
+    if (keys.length === 0) {
+      return
+    }
+
+    this.store.dispatch<ResourceAction>({ type: 'http:fetch:start', resourceKey: this.key, keys })
 
     Promise.resolve().then(fetchImpl).then(
       (payload) => {
-        this.store.dispatch<ResourceAction>({ type: 'http:fetch:complete', payload })
+        this.store.dispatch<ResourceAction>({ type: 'http:fetch:complete', resourceKey: this.key, payload })
       },
       (error) => {
-        this.store.dispatch<ResourceAction>({ type: 'http:fetch:failed', keys, error })
+        this.store.dispatch<ResourceAction>({ type: 'http:fetch:failed', resourceKey: this.key, keys, error })
       }
     )
   }
@@ -219,14 +227,14 @@ export class Resource<T> {
   }
 
   mutateMany(mutations: { [selector: string]: ResourceMutation<T> }, effect: () => Promise<any>): void {
-    this.store.dispatch<ResourceAction>({ type: 'http:mutation:start', payload: mutations })
+    this.store.dispatch<ResourceAction>({ type: 'http:mutation:start', resourceKey: this.key, payload: mutations })
 
     Promise.resolve().then(effect).then(
       () => {
-        this.store.dispatch<ResourceAction>({ type: 'http:mutation:complete', payload: mutations })
+        this.store.dispatch<ResourceAction>({ type: 'http:mutation:complete', resourceKey: this.key, payload: mutations })
       },
       (error) => {
-        this.store.dispatch<ResourceAction>({ type: 'http:mutation:failed', keys: Object.keys(mutations), error })
+        this.store.dispatch<ResourceAction>({ type: 'http:mutation:failed', resourceKey: this.key, keys: Object.keys(mutations), error })
       }
     )
   }
@@ -252,12 +260,12 @@ export function applyResourceMutation<T>(value: T, mutation?: ResourceMutation<T
 /** State */
 
 export type ResourceAction
-= { type: 'http:fetch:start', keys: string[] }
-| { type: 'http:fetch:complete', payload: { [key: string]: {} } }
-| { type: 'http:fetch:failed', keys: string[], error: Error }
-| { type: 'http:mutation:start', payload: { [key: string]: ResourceMutation<{}> } }
-| { type: 'http:mutation:complete', payload: { [key: string]: ResourceMutation<{}> } }
-| { type: 'http:mutation:failed', keys: string[], error: Error }
+= { type: 'http:fetch:start', resourceKey: string, keys: string[] }
+| { type: 'http:fetch:complete', resourceKey: string, payload: { [key: string]: {} } }
+| { type: 'http:fetch:failed', resourceKey: string, keys: string[], error: Error }
+| { type: 'http:mutation:start', resourceKey: string, payload: { [key: string]: ResourceMutation<{}> } }
+| { type: 'http:mutation:complete', resourceKey: string, payload: { [key: string]: ResourceMutation<{}> } }
+| { type: 'http:mutation:failed', resourceKey: string, keys: string[], error: Error }
 
 export type ResourceStateMap
 = { [key: string]: ResourceState<{}> | undefined }
@@ -273,161 +281,167 @@ export interface ResourceStateLoaded<T> { status: 'loaded'; value: T; mutation?:
 export interface ResourceStateFailed { status: 'failed'; error: Error }
 export interface ResourceStateDeleted { status: 'deleted' }
 
-export function reduceHTTPResource(state: ResourceStateMap = {}, action: ResourceAction | { type: '' }): ResourceStateMap {
-  if (action.type === 'http:fetch:start') {
-    return action.keys.reduce((state, key): ResourceStateMap => {
-      const resource = state[key]
+export function httpResourceReducer(key: string) {
+  return function reduceHTTPResource(state: ResourceStateMap = {}, action: ResourceAction | { type: '' }): ResourceStateMap {
+    if ((action as ResourceAction).resourceKey !== key) {
+      return state
+    }
 
-      if (!resource || resource.status !== 'loaded') {
-        return {
-          ...state,
-          [key]: {
-            status: 'loading'
+    if (action.type === 'http:fetch:start') {
+      return action.keys.reduce((state, key): ResourceStateMap => {
+        const resource = state[key]
+
+        if (!resource || resource.status !== 'loaded') {
+          return {
+            ...state,
+            [key]: {
+              status: 'loading'
+            }
+          }
+
+        } else {
+          return {
+            ...state,
+            [key]: {
+              ...resource,
+              reloading: true
+            }
           }
         }
+      }, state)
 
-      } else {
+    } else if (action.type === 'http:fetch:complete') {
+      const { payload } = action
+
+      return Object.keys(payload).reduce((state, key): ResourceStateMap => {
+        const value = payload[key]
+        const resource = state[key]
+
+        if (!resource || resource.status !== 'loaded') {
+          return {
+            ...state,
+            [key]: {
+              status: 'loaded',
+              value
+            }
+          }
+
+        } else {
+          return {
+            ...state,
+            [key]: stripUndefined({
+              ...resource,
+              reloading: undefined,
+              value
+            })
+          }
+        }
+      }, state)
+
+    } else if (action.type === 'http:fetch:failed') {
+      return action.keys.reduce((state, key): ResourceStateMap => {
+        const resource = state[key]
+
+        if (!resource || resource.status !== 'loaded') {
+          return {
+            ...state,
+            [key]: {
+              status: 'failed',
+              error: action.error
+            }
+          }
+        } else {
+          return {
+            ...state,
+            [key]: stripUndefined({
+              ...resource,
+              reloading: undefined,
+              error: action.error
+            })
+          }
+        }
+      }, state)
+
+    } else if (action.type === 'http:mutation:start') {
+      const { payload } = action
+
+      return Object.keys(payload).reduce((state, key): ResourceStateMap => {
+        const mutation = payload[key]
+        const resource = state[key]
+
+        if (!resource || resource.status !== 'loaded') {
+          throw new Error(`Resource ${key} must have been fetched before it is mutated`)
+        }
+
+        if (resource.mutation) {
+          throw new Error(`Resource ${key} already has a mutation pending`)
+        }
+
+        if (resource.reloading) {
+          throw new Error(`Resource ${key} cannot be mutated while is being fetched`)
+        }
+
         return {
           ...state,
           [key]: {
             ...resource,
-            reloading: true
+            mutation
           }
         }
-      }
-    }, state)
+      }, state)
 
-  } else if (action.type === 'http:fetch:complete') {
-    const { payload } = action
+    } else if (action.type === 'http:mutation:complete') {
+      const { payload } = action
 
-    return Object.keys(payload).reduce((state, key): ResourceStateMap => {
-      const value = payload[key]
-      const resource = state[key]
+      return Object.keys(payload).reduce((state, key): ResourceStateMap => {
+        const resource = state[key]
+        const mutation = payload[key]
 
-      if (!resource || resource.status !== 'loaded') {
-        return {
-          ...state,
-          [key]: {
-            status: 'loaded',
-            value
-          }
+        if (!resource || resource.status !== 'loaded') {
+          throw new Error(`Resource ${key} must have been fetched before it is mutated`)
         }
 
-      } else {
+        if (resource.reloading) {
+          throw new Error(`Resource ${key} cannot be mutated while is being fetched`)
+        }
+
+        if (mutation.type === 'delete') {
+          return {
+            ...state,
+            [key]: { status: 'deleted' }
+          }
+
+        } else {
+          return {
+            ...state,
+            [key]: stripUndefined({
+              ...resource,
+              mutation: undefined,
+              value: applyResourceMutation(resource.value, mutation)
+            })
+          }
+        }
+      }, state)
+
+    } else if (action.type === 'http:mutation:failed') {
+      // [todo] - Mutation errors aren't currently stored and passed back to the UI
+      console.error(action.error)
+
+      return action.keys.reduce((state, key): ResourceStateMap => {
+        const resource = state[key]
+
         return {
           ...state,
           [key]: stripUndefined({
             ...resource,
-            reloading: undefined,
-            value
+            mutation: undefined
           })
         }
-      }
-    }, state)
+      }, state)
+    }
 
-  } else if (action.type === 'http:fetch:failed') {
-    return action.keys.reduce((state, key): ResourceStateMap => {
-      const resource = state[key]
-
-      if (!resource || resource.status !== 'loaded') {
-        return {
-          ...state,
-          [key]: {
-            status: 'failed',
-            error: action.error
-          }
-        }
-      } else {
-        return {
-          ...state,
-          [key]: stripUndefined({
-            ...resource,
-            reloading: undefined,
-            error: action.error
-          })
-        }
-      }
-    }, state)
-
-  } else if (action.type === 'http:mutation:start') {
-    const { payload } = action
-
-    return Object.keys(payload).reduce((state, key): ResourceStateMap => {
-      const mutation = payload[key]
-      const resource = state[key]
-
-      if (!resource || resource.status !== 'loaded') {
-        throw new Error(`Resource ${key} must have been fetched before it is mutated`)
-      }
-
-      if (resource.mutation) {
-        throw new Error(`Resource ${key} already has a mutation pending`)
-      }
-
-      if (resource.reloading) {
-        throw new Error(`Resource ${key} cannot be mutated while is being fetched`)
-      }
-
-      return {
-        ...state,
-        [key]: {
-          ...resource,
-          mutation
-        }
-      }
-    }, state)
-
-  } else if (action.type === 'http:mutation:complete') {
-    const { payload } = action
-
-    return Object.keys(payload).reduce((state, key): ResourceStateMap => {
-      const resource = state[key]
-      const mutation = payload[key]
-
-      if (!resource || resource.status !== 'loaded') {
-        throw new Error(`Resource ${key} must have been fetched before it is mutated`)
-      }
-
-      if (resource.reloading) {
-        throw new Error(`Resource ${key} cannot be mutated while is being fetched`)
-      }
-
-      if (mutation.type === 'delete') {
-        return {
-          ...state,
-          [key]: { status: 'deleted' }
-        }
-
-      } else {
-        return {
-          ...state,
-          [key]: stripUndefined({
-            ...resource,
-            mutation: undefined,
-            value: applyResourceMutation(resource.value, mutation)
-          })
-        }
-      }
-    }, state)
-
-  } else if (action.type === 'http:mutation:failed') {
-    // [todo] - Mutation errors aren't currently stored and passed back to the UI
-    console.error(action.error)
-
-    return action.keys.reduce((state, key): ResourceStateMap => {
-      const resource = state[key]
-
-      return {
-        ...state,
-        [key]: stripUndefined({
-          ...resource,
-          mutation: undefined
-        })
-      }
-    }, state)
+    return state
   }
-
-  return state
 }
 
 function stripUndefined<T>(x: T): T {
