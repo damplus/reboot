5f99ae3025b5f72eacea57b0241ca8bf11f0183b
Chris Devereux <chrisd(a)damplus.com>
2017-01-10 10:41:49 +0000

    feat: Export HasState type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,7 @@ export {
   compose as composeMiddleware
 } from './middleware'
 
-export { BaseRequest } from './request'
+export { BaseRequest, HasState } from './request'
 export { MountRender, defaultResponse } from './response'
 export { Matcher, createMatcher } from './match'
 export { app, Route, AnyRoute } from './route'
